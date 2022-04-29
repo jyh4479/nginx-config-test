5444032727cbf425e1333cf1276c198c30e5e87b
Jeong yong hoon <jyh.dev@gmail.com>
2022-04-29 23:34:23 +0900

    UPLOAD INITAL SERVER

---
+++ nginx-api-server/src/main/java/com/example/nginxapiserver/NginxApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.nginxapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NginxApiServerApplication.class, args);
+    }
+
+}
