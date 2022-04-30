a9a0d29113641cc3727bc08620d83c91eb593228
Jeong yong hoon <jyh.dev@gmail.com>
2022-04-30 23:46:59 +0900

    UPDATE CLIENT
    Client Test 초기 환경 세팅

---
+++ nginx-client-server/src/component/ActionButton/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ActionButton = () => {
+    return (
+        <button>TEST</button>
+    )
+}
+
+export default ActionButton;
---
+++ nginx-client-server/src/container/pageContainer/HomePageContainer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ActionButton from "../../../component/ActionButton";
+
+const HomePageContainer = () => {
+    return (
+        <ActionButton/>
+    )
+}
+
+export default HomePageContainer;
