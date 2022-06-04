2ef5ec7409ed175b524c077d1205d01dfd820242
Jeong yong hoon <jyh.dev@gmail.com>
2022-06-04 15:42:56 +0900

    UPDATE
    간단한 연동 테스트

--- nginx-client-server/src/component/ActionButton/index.js
+++ nginx-client-server/src/component/ActionButton/index.js
@@ -1,9 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 const ActionButton = () => {
-    return (
-        <button>TEST</button>
-    )
+
+    const getServerString = () => {
+        testFetchApi().then(result => {
+            alert(result);
+        })
+    }
+
+    const testFetchApi = async () => {
+        const result = await axios.get("http://localhost:8080/action");
+        return result.data;
+    }
+
+    return (<button onClick={() => {
+        getServerString();
+    }}>TEST</button>)
 }
 
 export default ActionButton;
